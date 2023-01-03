98c17921c073955909b970638250a9afda026d18
Yonatan Salmon <yonatansalmon@itc.tech>
2023-01-03 11:53:53 +0200

    Logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
+const morgan = require('morgan');
 require('dotenv').config();
 const countriesRoute = require('./routes/countriesRoute');
 const usersRoute = require('./routes/usersRoute');
@@ -8,6 +9,7 @@ const dbConnection = require('./knex/knex');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(morgan('combined'))
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('images'))
